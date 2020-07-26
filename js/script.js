d249d3c63289bb063b808c80b9b44caf3d70590f
“Thiago” <thiagocosta130@gmail.com>
2020-07-26 17:29:07 -0300

    Add eventocaptura submit e validação de parâmetros de inputs

--- js/script.js
+++ js/script.js
@@ -3,4 +3,46 @@ $(document).ready(function () {
 		e.preventDefault();
 		$(this).tab('show');
 	});
+
+	//Adicionar Cliente (Evento)
+	$("#form-client").submit(function (e) {
+		e.preventDefault();
+		addClient();
+	});
 });
+
+///
+/// Clientes
+///
+
+//Adionar Cliente
+function addClient() {
+	var json = {};
+
+	json['name'] = $("#name").val();
+	json['email'] = $("#email").val();
+	json['phone'] = $("#phone").val();
+	json['cep'] = $("#cep").val();
+	json['address'] = $("#address").val();
+
+	var isValid = true;
+	for (key in json) {
+		if (!json[key] || typeof json[key] == undefined || json[key] == null) {
+			isValid = false;
+		}
+	}
+
+	json['number'] = parseInt($("#number").val());
+
+	if (!Number.isInteger(json['number'])) {
+		isValid = false;
+	}
+
+	json['complement'] = $("#complement").val();
+
+	if (!isValid) {
+		alert("Falha ao processar requisição. Parâmetros Inválidos.");
+	} else {
+		alert("Cliente Adicionado.");
+	}
+}
